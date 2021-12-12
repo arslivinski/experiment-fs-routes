e760ed4974f7cd8b7939c9d253447a9a9cfd8764
Anderson Slivinski <11060804+arslivinski@users.noreply.github.com>
2021-12-12 16:34:08 -0300

    chore: configure linter with ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const OFF = 'off';
+const ERROR = 'error';
+
+module.exports = {
+  root: true,
+  extends: [
+    '@arslivinski/eslint-config',
+    '@arslivinski/eslint-config/react',
+  ],
+  rules: {
+    'import/no-unassigned-import': [
+      ERROR,
+      {
+        allow: [
+          '**/*.css',
+        ],
+      },
+    ],
+  },
+  overrides: [
+    {
+      files: [
+        './.*.js',
+        './*.js',
+        './scripts/**/*.js',
+      ],
+      env: {
+        node: true,
+      },
+      parserOptions: {
+        sourceType: 'script',
+      },
+      rules: {
+        'no-console': OFF,
+        'strict': [ERROR, 'global'],
+        'import/no-commonjs': OFF,
+        'import/no-extraneous-dependencies': [
+          ERROR,
+          {
+            devDependencies: true,
+            optionalDependencies: false,
+            peerDependencies: false,
+          },
+        ],
+        'import/no-nodejs-modules': OFF,
+      },
+    },
+  ],
+};
